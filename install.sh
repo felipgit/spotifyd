a1f1b1c19f0474b6a0c594353fe30c239419cea7
felipgit <51529268+felipgit@users.noreply.github.com>
2020-04-05 20:39:08 +0200

    Create install.sh
    steps to install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://github.com/Spotifyd/spotifyd/releases/download/v0.2.24/spotifyd-linux-armv6-slim.tar.gz
+tar zxf spotifyd-linux-armv6-slim.tar.gz 
+sudo mv spotifyd /usr/bin/
+sudo mkdir /etc/spotifyd
+sudo wget https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service -O /etc/systemd/system/spotifyd.servicesudo
+sudo vi /etc/systemd/system/spotifyd.servicesudo
+sudo vi /etc/spotifyd/spotifyd.conf
+sudo systemctl daemon-reload
+sudo systemctl start spotifyd.service 
+sudo systemctl enable spotifyd.service 
+
