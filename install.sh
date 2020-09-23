df769c08d088a62d4eb7110fc0d481a1e8a3f05f
felipgit <51529268+felipgit@users.noreply.github.com>
2020-09-23 09:09:58 +0200

    Update install.sh
    sudo typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ tar zxf spotifyd-linux-armv6-slim.tar.gz
 sudo mv spotifyd /usr/bin/
 sudo mkdir /etc/spotifyd
 sudo wget https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service -O /etc/systemd/system/spotifyd.servicesudo
-sudo vi /etc/systemd/system/spotifyd.servicesudo
+sudo vi /etc/systemd/system/spotifyd.service
 sudo vi /etc/spotifyd/spotifyd.conf
 sudo systemctl daemon-reload
 sudo systemctl start spotifyd.service 
